822bddbb0b29ea5565df2a0e4145d47dfeb57660
Frazer Lillie <frazerlillie@intelligencefusion.co.uk>
2025-03-19 16:39:44 +0000

    Added additional config var

--- config/console.php
+++ config/console.php
@@ -2,5 +2,6 @@
 
 return [
     'postcode_url_to_import' => env('POSTCODE_IMPORT_URL'),
-    'postcode_file_path' => env('POSTCODE_FILE_PATH'),
+    'postcode_local_file_path' => env('POSTCODE_LOCAL_FILE_PATH'),
+    'postcode_filename' => env('POSTCODE_FILENAME'),
 ];
