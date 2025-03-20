caa4de8f7e198db8d286f0ea0c27bd043eedf62e
Frazer Lillie <frazerlillie@intelligencefusion.co.uk>
2025-03-20 17:29:57 +0000

    Added DTO test

---
+++ tests/Unit/DTO/NearbyStoreRequestDTOTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Unit\DTO;
+
+use App\DTO\NearbyStoreRequestDTO;
+use PHPUnit\Framework\TestCase;
+
+class NearbyStoreRequestDTOTest extends TestCase
+{
+    public function testNearbyStoreRequestDTOInitialisation()
+    {
+        $data = [
+            'latitude' => '51.5074',
+            'longitude' => 0.1278,
+            'radius' => 5,
+        ];
+
+        $dto = new NearbyStoreRequestDTO($data);
+
+        $this->assertEquals(51.5074, $dto->latitude);
+        $this->assertEquals(0.1278, $dto->longitude);
+        $this->assertEquals(5, $dto->radius);
+    }
+}
